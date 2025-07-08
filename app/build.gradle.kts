2429b6f3521b02656b2907f781446d8ac18e6c11
mike <krasnoff.mike@yandex.ru>
2025-07-08 23:40:11 +0300

    init commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    application
+    checkstyle
+    id("org.sonarqube") version "6.0.1.5171"
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
+    id("com.github.ben-manes.versions") version "0.52.0"
+    jacoco
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "thygh0st_java-project-78")
+        property("sonar.organization", "thygh0st")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property ("sonar.coverage.jacoco.xmlReportPaths", "/reports/jacoco/test/jacocoTestReport.xml")
+    }
+}
+
+application {
+    mainClass = "hexlet.code.App"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.13.0-M2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+    reports {
+        xml.required = true
+        csv.required = false
+        html.outputLocation = layout.buildDirectory.dir("jacocoHtml")
+    }
+}
